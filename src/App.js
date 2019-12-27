8067300151b0887c2d2e9f989456cf2f8de971f2
Bryan <bryan@fiddle.io>
2019-12-27 22:44:18 +0800

    testdrive

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const winningCombinations = [
+    //horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    //vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    //diagonal
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const [theArray, setTheArray] = useState([...Array(9)].map(_ => ''));
+  const [player, togglePlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  const resetAll = () => {
+    setTheArray([...Array(9)].map(_ => ''));
+    togglePlayer('X');
+    setWinner(null);
+  };
+
+  const setSelectedBox = index => {
+    if (winner) {
+      resetAll();
+    } else {
+      const arrayCopy = [...theArray];
+      if (player === 'X') {
+        arrayCopy[index] = 'X';
+        setTheArray(arrayCopy);
+        checkForWinner(arrayCopy);
+        togglePlayer('O');
+      } else {
+        arrayCopy[index] = 'O';
+        setTheArray(arrayCopy);
+        checkForWinner(arrayCopy);
+        togglePlayer('X');
+      }
+    }
+  };
+
+  const checkForWinner = arrayCopy => {
+    for (let key of winningCombinations) {
+      const [first, second, third] = key;
+      if (
+        arrayCopy[first] === arrayCopy[second] &&
+        arrayCopy[second] === arrayCopy[third] &&
+        (arrayCopy[first] !== '' ||
+          arrayCopy[second] !== '' ||
+          arrayCopy[third] !== '')
+      ) {
+        setWinner(player);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Tic Tac Toe</div>
+      <div className="main-container">
+        {theArray.map((value, index) => {
+          return (
+            <TicBox
+              setSelectedBox={() => setSelectedBox(index)}
+              value={value}
+              key={index}
+            />
+          );
+        })}
+      </div>
+      {winner ? (
+        <div>{`PLAYER ${winner} WINS`}</div>
+      ) : (
+        <div>{`It's player ${player} turn`}</div>
+      )}
+      <button onClick={() => resetAll()}>RESET</button>
     </div>
   );
 }
 
 export default App;
+
+const TicBox = ({ setSelectedBox, value }) => {
+  return (
+    <div className="box" onClick={setSelectedBox}>
+      {value}
+    </div>
+  );
+};
